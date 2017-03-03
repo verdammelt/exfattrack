d75236b40db2e1d0da78acee925f6bd9473d6343
Mark Simpson <verdammelt@gmail.com>
2017-03-03 09:08:57 -0500

    Adding stub web & biz-logic apps.

---
+++ apps/weights/lib/weights.ex
@@ -0,0 +1,18 @@
+defmodule Weights do
+  @moduledoc """
+  Documentation for Weights.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Weights.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ apps/weights/test/weights_test.exs
@@ -0,0 +1,8 @@
+defmodule WeightsTest do
+  use ExUnit.Case
+  doctest Weights
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
