bf3e86989eceab3b9b103eb273c9c5a8fdc56756
Mark Simpson <verdammelt@gmail.com>
2017-03-03 17:11:14 -0500

    Use :utc_datetime in database. :datetime is deprecated.

--- apps/web/priv/repo/migrations/20170303192748_create_coherence_user.exs
+++ apps/web/priv/repo/migrations/20170303192748_create_coherence_user.exs
@@ -6,26 +6,26 @@ defmodule Web.Repo.Migrations.CreateCoherenceUser do
       add :email, :string
       # trackable
       add :sign_in_count, :integer, default: 0
-      add :current_sign_in_at, :datetime
-      add :last_sign_in_at, :datetime
+      add :current_sign_in_at, :utc_datetime
+      add :last_sign_in_at, :utc_datetime
       add :current_sign_in_ip, :string
       add :last_sign_in_ip, :string
       # rememberable
-      add :remember_created_at, :datetime
+      add :remember_created_at, :utc_datetime
       # authenticatable
       add :password_hash, :string
       # recoverable
       add :reset_password_token, :string
-      add :reset_password_sent_at, :datetime
+      add :reset_password_sent_at, :utc_datetime
       # lockable
       add :failed_attempts, :integer, default: 0
-      add :locked_at, :datetime
+      add :locked_at, :utc_datetime
       # unlockable_with_token
       add :unlock_token, :string
       # confirmable
       add :confirmation_token, :string
-      add :confirmed_at, :datetime
-      add :confirmation_sent_at, :datetime
+      add :confirmed_at, :utc_datetime
+      add :confirmation_sent_at, :utc_datetime
 
       timestamps()
     end
